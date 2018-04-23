0d0de9cbf434447eece1db01c401280d34224e40
weiping <wepa@microsoft.com>
2018-04-23 14:59:23 +0800

    add details into sidebar

--- gateway-server/src/main/webapp/src/App.vue
+++ gateway-server/src/main/webapp/src/App.vue
@@ -1,15 +1,9 @@
 <template>
     <div>
         <Nav></Nav>
-        <b-container fluid>
-            <b-row class="text-center">
-                <b-col>
-                    <Sidebar></Sidebar>
-                </b-col>
-                <b-col cols="8"></b-col>
-                <b-col></b-col>
-            </b-row>
-        </b-container>
+        <div>
+            <Sidebar></Sidebar>
+        </div>
     </div>
 </template>
 
--- gateway-server/src/main/webapp/src/components/Sidebar.vue
+++ gateway-server/src/main/webapp/src/components/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Left Sidebar
+    <div class="sidenav">
+        <a href="about">About</a>
+        <a href="azure-spring-cosmosdb">Azure CosmosDB</a>
+        <a href="contact">Contact</a>
     </div>
 </template>
 <script>
@@ -13,3 +15,39 @@
         }
     };
 </script>
+<style>
+    .sidenav {
+        height: 100%;
+        width: 240px;
+        position: fixed;
+        z-index: 1;
+        left: 0;
+        background-color: #f9f9f9;
+        overflow-x: hidden;
+        padding-top: 20px;
+        text-align: center;
+    }
+
+    .sidenav a {
+        padding: 6px 8px 6px 16px;
+        text-decoration: none;
+        font-size: 1rem;
+        color: #343a40;
+        display: block;
+    }
+
+    .sidenav a:hover {
+        color: #727477;
+    }
+
+    .main {
+        margin-left: 160px;
+        font-size: 28px;
+        padding: 0px 10px;
+    }
+
+    @media screen and (max-height: 450px) {
+        .sidenav {padding-top: 15px;}
+        .sidenav a {font-size: 18px;}
+    }
+</style>
