fc6446170cbcb37523fdc39b0e3e1ae7543b07a4
weiping <wepa@microsoft.com>
2018-04-23 14:09:37 +0800

    add nav and sidebar

--- gateway-server/src/main/webapp/src/App.vue
+++ gateway-server/src/main/webapp/src/App.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
-        {{ msg }}
+        <Nav></Nav>
+        <b-container fluid>
+            <b-row class="text-center">
+                <b-col>
+                    <Sidebar></Sidebar>
+                </b-col>
+                <b-col cols="8"></b-col>
+                <b-col></b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import { Layout } from 'bootstrap-vue/es/components'
+    import Nav from './components/Nav'
+    import Sidebar from './components/Sidebar'
+
+    Vue.use(Layout);
+
     export default {
         data: function() {
             return {
-                msg: 'Hello World'
             }
+        },
+        components: {
+            Nav,
+            Sidebar
         }
     };
 </script>
---
+++ gateway-server/src/main/webapp/src/components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Left Sidebar
+    </div>
+</template>
+<script>
+    export default {
+        data: function() {
+            return {
+            }
+        },
+        methods: {
+        }
+    };
+</script>
